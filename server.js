a579b60c1aada5972034a193b0e785f00956a4d4
Sam <bezzantsam@gmail.com>
2022-07-04 03:39:35 -0700

    multer

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const multer = require('multer');
+
 const express = require("express")
 const app = express()
+const upload = multer({dest: "uploads"})
 
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+app.post('/upload', upload.single("file"),  (req, res) => {
+  res.send("hi")
 })
 
 app.listen(3000)
