f1e2e78dca7e82301fc575b3fba0c8b8ef72a7e2
Sam <bezzantsam@gmail.com>
2022-07-04 04:07:17 -0700

    async bcrypt hash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const multer = require('multer');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const express = require("express")
 const app = express()
 const upload = multer({dest: "uploads"})
@@ -13,8 +13,15 @@ app.set("view engine", "ejs")
 app.get("/", (req, res) => {
     res.render("index")
 })
-app.post('/upload', upload.single("file"),  (req, res) => {
-  res.send("hi")
+app.post('/upload', upload.single("file"),  async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname
+  }
+    if (req.body.password != null && req.body.password != '' )
+    {
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
 })
 
 app.listen(process.env.PORT)
