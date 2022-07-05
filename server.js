a2f5e3bb1046d27bcf3f05a5f5319bb9355ac11d
Sam <bezzantsam@gmail.com>
2022-07-05 04:38:22 -0700

    handleDownload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express")
 const app = express()
 const upload = multer({dest: "uploads"})
 const File = require("./models/File")
+app.use(express.urlencoded({extended: true}))
 
 mongoose.connect(process.env.DATABASE_URL)
 
@@ -32,9 +33,41 @@ app.get("/file/:id", async (req, res) => {
   res.send(req.params.id)
   const file = await File.findById(req.params.id)
   
+  if(file.password != null){
+    if(req.body.password == null){
+      res.render("password")
+      return
+    }
+    if(!await bcrypt.compare(req.body.password, file.password)){
+      res.render("password", {error: true})
+      return
+    }
+    }
+
+  
   file.downloadCount++
   await file.save()
+  res.download(file.path, file.originalName)
 
 })
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
+  
+  if(file.password != null){
+    if(req.body.password == null){
+      res.render("password")
+      return
+    }
+    if(!await bcrypt.compare(req.body.password, file.password)){
+      res.render("password", {error: true})
+      return
+    }
+    }
 
+  
+  file.downloadCount++
+  await file.save()
+  res.download(file.path, file.originalName)
+
+}
 app.listen(process.env.PORT)
