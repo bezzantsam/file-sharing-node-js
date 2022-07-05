c5574bbd91cf25700db7f18ef5ccc93a3c18e41f
Sam <bezzantsam@gmail.com>
2022-07-05 03:27:19 -0700

    minor change

--- server.js
+++ server.js
@@ -28,7 +28,12 @@ app.post('/upload', upload.single("file"), async (req, res) => {
     res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
 })
 
-app.get("/file/:id", (req, res) => {
+app.get("/file/:id", async (req, res) => {
+  res.send(req.params.id)
+  const file = await File.findById(req.params.id)
+  
+  file.downloadCount++
+  await file.save()
 
 })
 
