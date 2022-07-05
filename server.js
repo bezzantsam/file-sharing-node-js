1a15e97b1dbd07b5d861b91ccec5d2fe97ba9ee5
Sam <bezzantsam@gmail.com>
2022-07-05 04:42:40 -0700

    route to clean up crud

--- server.js
+++ server.js
@@ -28,28 +28,8 @@ app.post('/upload', upload.single("file"), async (req, res) => {
     
     res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
 })
+app.route("/file/:id").get(handleDownload).post(handleDownload)
 
-app.get("/file/:id", async (req, res) => {
-  res.send(req.params.id)
-  const file = await File.findById(req.params.id)
-  
-  if(file.password != null){
-    if(req.body.password == null){
-      res.render("password")
-      return
-    }
-    if(!await bcrypt.compare(req.body.password, file.password)){
-      res.render("password", {error: true})
-      return
-    }
-    }
-
-  
-  file.downloadCount++
-  await file.save()
-  res.download(file.path, file.originalName)
-
-})
 async function handleDownload(req, res) {
   const file = await File.findById(req.params.id)
   
